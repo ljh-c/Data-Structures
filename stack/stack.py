7d3af8327316d396ac05356e18787cafb9e95e50
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-05-11 22:12:04 -0700

    Complete MVP exercises: singly linked list, stack, queue

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,54 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+from singly_linked_list import LinkedList
+
+# array implementation
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.storage.append(value)
+#         self.size += 1
+
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+#         else:
+#             self.size -= 1
+#             return self.storage.pop()
+
+# linked list implementation
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
-
+        return self.size
+    
     def push(self, value):
-        pass
-
+        self.storage.add_to_tail(value)
+        self.size += 1
+    
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        elif self.size == 1:
+            self.size = 0
+            return self.storage.remove_from_head()
+        else:
+            # traverse linked list to get to tail
+            current = self.storage.head
+            while current.get_next().get_next() is not None:
+                current = current.get_next()
+            value = current.get_next().get_value()
+            current.set_next(None)
+            self.size -= 1
+            return value
